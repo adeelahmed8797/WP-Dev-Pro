062f4b10e20e8f403f7178a6491a0071c00e90f8
Adeel Ahmed <adeelahmed8797@gmail.com>
2026-05-18 20:42:16 +0500

    Delete um-theme-h2h-child directory

--- um-theme-h2h-child/functions.php
+++
@@ -1,6 +0,0 @@
-<?php
-add_action( 'wp_enqueue_scripts', 'h2h_child_enqueue_styles' );
-function h2h_child_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'h2h-um-branding', get_stylesheet_directory_uri() . '/h2h-um-branding.css', array( 'parent-style' ), '1.0.0' );
-}
